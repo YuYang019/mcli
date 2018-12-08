318df7794ea3d6735803252cdc8fb4db56c15170
maoyuyang <yuyangmao@sf-express.com>
2018-12-08 17:40:06 +0800

    use cac instead of commander

--- bin/mcli.js
+++ bin/mcli.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
 
-const commander = require('commander')
+const cli = require('cac')()
 const packageInfo = require('../package.json');
+const init = require('../lib/init');
 
-commander
+// []表示可选参数，<>表示必须参数
+
+cli
   .version(packageInfo.version)
-  .usage('<command> [项目名称]')
-  .command('init', '创建新项目')
-  .parse(process.argv)
+  .help()
+
+cli
+  .command('init <projectName>', 'init project')
+  .option('-c, --clone', 'git clone the project rather than download it')
+  .action((projectName, options) => {
+    const { clone } = options
+    
+    wrapCommand(init)({
+      projectName,
+      clone
+    })
+  })
+
+function wrapCommand(fn) {
+  return (...args) => {
+    return fn(...args)
+  }
+}
+
+cli.parse()
+
